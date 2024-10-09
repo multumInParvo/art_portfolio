6d0e791958e7e39c9b830ece3266ddd0d4cca02d
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-09 11:57:31 +0200

    Rename painting details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,31 @@ import Slider from '../app/components/Slider'
 const paintings = [
   {
     src: "/images/azaylerideau.webp",
-    title: "Levitation",
+    title: "Azay-le-Rideau",
     description: "The house with red doors",
-    dimensions: "60 x 68 inches, oil on canvas",
+    dimensions: "60 x 80 cm, oil on canvas",
     year: 2024
   },
   {
     src: "/images/campestre.webp",
-    title: "Camembert",
-    description: "An abstract painting with warm colors",
-    dimensions: "60 x 68 inches, oil on canvas",
-    year: 2024
+    title: "Campestre",
+    description: "Cheese and Milk",
+    dimensions: "30 x 40 cm, oil on canvas",
+    year: 2022
   },
   {
     src: "/images/sixpm.webp",
-    title: "Levitation",
-    description: "A jar with cheese",
-    dimensions: "60 x 68 inches, oil on canvas",
+    title: "At 6PM",
+    description: "Red Chicken",
+    dimensions: "60 x 80 cm inches, oil on canvas",
     year: 2024
   },
   {
     src: "/images/campestre_2.webp",
-    title: "Levitation",
+    title: "Campestre 2",
     description: "Apples",
-    dimensions: "60 x 68 inches, oil on canvas",
-    year: 2024
+    dimensions: "30 x 40 cm, oil on canvas",
+    year: 2022
   }
   // Add more paintings here
 ]
