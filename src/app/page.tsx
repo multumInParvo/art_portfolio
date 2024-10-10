831bdb45b24834f03826f6b2e7cb85eba7452cf5
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-10 23:37:53 +0200

    Update Home component to use Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,6 @@
+import { paintings } from '../app/data/paintings'
 import Slider from '../app/components/Slider'
 
-const paintings = [
-  {
-    src: "/images/azaylerideau.webp",
-    title: "Azay-le-Rideau",
-    description: "The house with red doors",
-    dimensions: "60 x 80 cm, oil on canvas",
-    year: 2024
-  },
-  {
-    src: "/images/campestre.webp",
-    title: "Campestre",
-    description: "Cheese and Milk",
-    dimensions: "30 x 40 cm, oil on canvas",
-    year: 2022
-  },
-  {
-    src: "/images/sixpm.webp",
-    title: "At 6PM",
-    description: "Red Chicken",
-    dimensions: "60 x 80 cm inches, oil on canvas",
-    year: 2024
-  },
-  {
-    src: "/images/campestre_2.webp",
-    title: "Campestre 2",
-    description: "Apples",
-    dimensions: "30 x 40 cm, oil on canvas",
-    year: 2022
-  }
-  // Add more paintings here
-]
-
 export default function Home() {
-  return (
-    <Slider paintings={paintings} />
-  )
+  return <Slider paintings={paintings} />
 }
