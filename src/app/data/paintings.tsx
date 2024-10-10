7fd1d939e81ea934e5b4abe8f0dec0475d9686a2
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-10 23:41:23 +0200

    Add paintings data file

---
+++ src/app/data/paintings.tsx
@@ -0,0 +1,32 @@
+// paintings.ts
+
+export const paintings = [
+    {
+        src: "/images/azaylerideau.webp",
+        title: "Azay-le-Rideau",
+        description: "The house with red doors",
+        dimensions: "60 x 80 cm, oil on canvas",
+        year: 2024
+    },
+    {
+        src: "/images/campestre.webp",
+        title: "Campestre",
+        description: "Cheese and Milk",
+        dimensions: "30 x 40 cm, oil on canvas",
+        year: 2022
+    },
+    {
+        src: "/images/sixpm.webp",
+        title: "At 6PM",
+        description: "Red Chicken",
+        dimensions: "60 x 80 cm, oil on canvas",
+        year: 2024
+    },
+    {
+        src: "/images/campestre_2.webp",
+        title: "Campestre 2",
+        description: "Apples",
+        dimensions: "30 x 40 cm, oil on canvas",
+        year: 2022
+    }
+];
