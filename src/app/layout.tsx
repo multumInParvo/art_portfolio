ba240465d0062b595c5d4fe5c91a0f2f0cbeed82
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-10 23:36:39 +0200

    Separate server and client components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-// src/app/layout.tsx
 import { Cinzel } from 'next/font/google';
-import Link from 'next/link';
 import './globals.css';
+import { PaintingProvider } from './context/PaintingContext';
+import ClientLayout from '../app/components/ClientLayout';
 
 export const metadata = {
   title: 'Art Portfolio',
@@ -10,7 +10,7 @@ export const metadata = {
 
 const cinzel = Cinzel({
   subsets: ['latin'],
-  weight: ['400', '700'], // Choose the weights you need
+  weight: ['400', '700'],
 });
 
 export default function RootLayout({
@@ -20,38 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex h-screen">
-          {/* Left Side Menu */}
-          <aside className='w-1/4 p-10 flex flex-col'>
-
-            <div className="h-1/2 space-y-8">
-              <div className='space-y-1'>
-                <Link href="/" className="uppercase font-cinzel text-3xl text-gray-700">Oleksandr <br /> Pryvalov</Link>
-                <p className="text-gray-600 italic font-cinzel tracking-widest">paintings</p>
-              </div>
-              <div className="text-lg flex flex-col space-y-2">
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
-              </div>
-            </div>
-
-            <div className="flex flex-col justify-end items-start h-1/2 space-y-3">
-              <h2 id="paintingTitle" className="text-xl font-semibold font-cinzel text-gray-700"></h2>
-              <div className='text-sm'>
-                <p id="paintingDescription"></p>
-                <p id="paintingDimensions"></p>
-                <p id="paintingYear"></p>
-              </div>
-            </div>
-
-          </aside>
-
-          {/* Right Side Content */}
-          <main className="p-10 w-3/4">
-            {children}
-          </main>
-        </div>
+      <body className={cinzel.className}>
+        <PaintingProvider>
+          <ClientLayout>{children}</ClientLayout>
+        </PaintingProvider>
       </body>
     </html>
   );
