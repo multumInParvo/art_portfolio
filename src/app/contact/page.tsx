3419fa6d763d713347ac3aaf892c808e953891f8
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-12 12:32:13 +0200

    Remove unecessary styles

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { Form } from '../components/Form';
 
 export default function ContactPage() {
     return (
-        <div className="flex flex-col items-start">
+        <div className="flex flex-col items-center">
             <h1 className="text-6xl font-extrabold opacity-10 mb-12">Contact</h1>
             <Form />
         </div>
