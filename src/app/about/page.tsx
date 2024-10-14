4630400092e3eb63069fbc3661f0451c5379d56d
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-14 22:23:00 +0200

    Style about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,23 @@
-// src/app/about/page.tsx
+import Image from 'next/image';
+
 export default function AboutPage() {
-    return (
-      <div>
-        <h1 className="text-3xl font-bold">About</h1>
-        Oleksandr Pryvalov (born 1987) spent his childhood in Mykolaiv, Ukraine, but his artistic passion grew in Argentina after immigrating there with his parents. While exploring various interests in his youth, art, particularly drawing and painting, remained a constant. As a self-taught artist, Pryvalov&apos;s fascination with capturing the world visually became a defining force in his life. Finally, in France, he met the conditions to take oil painting to a serious level.
+  return (
+    <div className="flex flex-col items-left w-full ml-16">
+      <h1 className="text-2xl mb-20">About</h1>
+      <div className="flex flex-row w-full items-start p-0 gap-16">
+        <p className="font-proxima max-w-2xl text-start leading-loose">
+          Oleksandr Pryvalov (born 1987) spent his childhood in Mykolaiv, Ukraine, but his artistic passion grew in Argentina after immigrating there with his parents. 
+          While exploring various interests in his youth, art, particularly drawing and painting, remained a constant. 
+          As a self-taught artist, Pryvalov&apos;s fascination with capturing the world visually became a defining force in his life. 
+          Finally, in France, he met the conditions to take oil painting to a serious level.
+        </p>
+        <Image
+          src="/images/artist.webp"
+          alt="Artist"
+          width={384} 
+          height={384} 
+        />
       </div>
-    );
-  }
+    </div>
+  );
+}
