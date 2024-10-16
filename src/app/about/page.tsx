2d86e7b0c82a1777d0d8f097a1cd5a8c0513e484
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-16 20:00:23 +0200

    Add provisional styling to About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <div className="flex flex-col items-left w-full ml-10">
       <h1 className="text-2xl mb-20">About</h1>
-      <div className="flex flex-row w-full items-start p-0 gap-14">
+      <div className="flex flex-col w-full items-start p-0 gap-14">
         <p className="font-proxima max-w-2xl text-start leading-loose">
           Oleksandr Pryvalov (born 1987) spent his childhood in Mykolaiv, Ukraine, but his artistic passion grew in Argentina after immigrating there with his parents. 
           While exploring various interests in his youth, art, particularly drawing and painting, remained a constant. 
@@ -14,8 +14,8 @@ export default function AboutPage() {
         <Image
           src="/images/artist.webp"
           alt="Artist"
-          width={384} 
-          height={384} 
+          width={364} 
+          height={364} 
         />
       </div>
     </div>
