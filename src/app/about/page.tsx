f767d7fcf9349887ff31d6dc6fff866cfd09d875
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-26 15:42:05 +0200

    Fix responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image';
 
 export default function AboutPage() {
   return (
-    <div className="w-full">
-      <h1 className='text-2xl mb-2 mt-0 font-playfair md:text-3xl md:mb-10 border-b'>
-        About
-      </h1>
-      
+    <div className="w-full flex flex-col items-center md:ml-16 md:max-w-3xl">
+      <div className="w-full">
+        <h1 className='text-2xl mb-2 mt-0 font-playfair md:text-3xl md:mb-10 border-b'>
+          About
+        </h1>
+      </div>
+
       <div className="flex flex-col gap-8 md:gap-14 mt-6">
         <div className="w-full">
           <p className="text-sm md:text-base font-semibold text-gray-700 leading-relaxed font-nunito max-w-full break-words">
@@ -16,7 +18,7 @@ export default function AboutPage() {
             Finally, in France, he met the conditions to take oil painting to a serious level.
           </p>
         </div>
-        
+
         <div className="w-full flex justify-center">
           <div className="relative w-full max-w-[364px]">
             <Image
