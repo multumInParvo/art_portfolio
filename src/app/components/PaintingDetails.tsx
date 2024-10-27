922780de73afe1952cb4bde6ee57c99a8ca4f7dc
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-27 23:44:09 +0100

    Fix color for painting details

--- src/app/components/PaintingDetails.tsx
+++ src/app/components/PaintingDetails.tsx
@@ -10,25 +10,38 @@ const PaintingDetails = React.memo(function PaintingDetails() {
 
   return (
     <div className="hidden md:block">
-      <h2 className="text-lg font-bold mb-1 font-nunito text-gray-600">
+      <h2 className="text-lg font-bold mb-1 font-nunito text-gray-600 dark:text-gray-100">
         {currentPainting.title}
       </h2>
-        <p className="text-sm font-nunito">{currentPainting.dimensions}</p>
-        <p className="text-sm font-nunito">{currentPainting.year}</p>
+      <p className="text-sm font-nunito text-gray-600 dark:text-gray-300">
+        {currentPainting.dimensions}
+      </p>
+      <p className="text-sm font-nunito text-gray-600 dark:text-gray-300">
+        {currentPainting.year}
+      </p>
       <div className="mt-2 text-sm hidden md:block">
-        <button
-          onClick={goToPrevious}
-          className="hover:underline font-nunito font-bold text-gray-600"
-        >
-          PREV
-        </button>
-        <span className="mx-1 font-nunito font-bold">/</span>
-        <button
-          onClick={goToNext}
-          className="hover:underline font-nunito font-bold text-gray-600"
-        >
-          NEXT
-        </button>
+      <button
+  onClick={goToPrevious}
+  className="relative inline-block font-nunito font-bold text-gray-600 dark:text-gray-300 transition-all
+    after:content-[''] after:absolute after:-bottom-1 after:left-0 after:w-full after:h-[2px] after:scale-x-0
+    dark:after:bg-darkGold after:bg-transparent after:transition-transform after:duration-300
+    hover:after:scale-x-100"
+>
+  PREV
+</button>
+<span className="mx-1 font-nunito font-bold text-gray-600 dark:text-gray-300">
+  /
+</span>
+<button
+  onClick={goToNext}
+  className="relative inline-block font-nunito font-bold text-gray-600 dark:text-gray-300 transition-all
+    after:content-[''] after:absolute after:-bottom-1 after:left-0 after:w-full after:h-[2px] after:scale-x-0
+    dark:after:bg-darkGold after:bg-transparent after:transition-transform after:duration-300
+    hover:after:scale-x-100"
+>
+  NEXT
+</button>
+
       </div>
     </div>
   );
