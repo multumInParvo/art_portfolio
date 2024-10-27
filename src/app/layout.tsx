4470350d8be983106095b4236ae5bf70850e6e2e
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-10-27 23:39:19 +0100

    Wrap Layout in ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
+// app/layout.tsx
 import { Playfair_Display, Nunito } from 'next/font/google';
 import './globals.css';
 import { PaintingProvider } from './context/PaintingContext';
+import { ThemeProvider } from '../app/context/ThemeContext';
 import ClientLayout from '../app/components/ClientLayout';
 
 export const metadata = {
   title: 'Oleksandr Pryvalov',
   description: 'My personal art portfolio',
   icons: {
-    icon: '/favicon.ico', 
+    icon: '/favicon.ico',
   },
 };
 
@@ -15,6 +17,7 @@ const playfair = Playfair_Display({
   subsets: ['latin'],
   variable: '--font-playfair',
 });
+
 const nunito = Nunito({
   subsets: ['latin'],
   variable: '--font-nunito'
@@ -28,9 +31,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${nunito.variable}`}>
       <body>
-        <PaintingProvider>
-          <ClientLayout isLoading={false}>{children}</ClientLayout>
-        </PaintingProvider>
+        <ThemeProvider>
+          <PaintingProvider>
+            <ClientLayout isLoading={false}>{children}</ClientLayout>
+          </PaintingProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
