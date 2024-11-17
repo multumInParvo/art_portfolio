e7e9d609a18047dc2453c86f946db14614ea87de
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-11-17 20:14:31 +0100

    Add SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// app/layout.tsx
+import { Metadata } from 'next';
 import { Playfair_Display, Nunito } from 'next/font/google';
 import './globals.css';
 import { PaintingProvider } from './context/PaintingContext';
@@ -6,11 +6,46 @@ import { ThemeProvider } from '../app/context/ThemeContext';
 import { LanguageProvider } from './context/LanguageContext';
 import LayoutWrapper from './components/LayoutWrapper';
 
-export const metadata = {
-  title: 'Oleksandr Pryvalov',
-  description: 'My personal art portfolio',
-  icons: {
-    icon: '/favicon.ico',
+export const metadata: Metadata = {
+  metadataBase: new URL('https://oleksandrpryvalov.art/'),
+  title: {
+    default: 'Oleksandr Pryvalov | Artiste Peintre - Artist',
+    template: '%s | Oleksandr Pryvalov'
+  },
+  description: 'Découvrez le portfolio artistique d\'Oleksandr Pryvalov, artiste peintre basé en France. Explore the artistic portfolio of Oleksandr Pryvalov, artist based in France.',
+  keywords: ['artiste peintre', 'artist', 'paintings', 'peintures', 'art contemporain', 'contemporary art', 'France', 'Oleksandr Pryvalov'],
+  creator: 'Oleksandr Pryvalov',
+  openGraph: {
+    type: 'website',
+    locale: 'fr_FR',
+    alternateLocale: 'en_US',
+    title: 'Oleksandr Pryvalov | Artiste Peintre - Artist',
+    description: 'Découvrez le portfolio artistique d\'Oleksandr Pryvalov, artiste peintre basé en France. Explore the artistic portfolio of Oleksandr Pryvalov, artist based in France.',
+    siteName: 'Oleksandr Pryvalov Art',
+    // We'll use your most representative painting as the OG image
+    images: [
+      {
+        url: '/images/leaving_the_nest.webp', // You'll need to add your best painting image to the public folder
+        width: 1200,
+        height: 630,
+        alt: 'Oeuvre d\'art par Oleksandr Pryvalov - Artwork by Oleksandr Pryvalov'
+      }
+    ]
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+    },
+  },
+  alternates: {
+    languages: {
+      'en': '/',
+      'fr': '/fr',
+    },
   },
 };
 
@@ -31,11 +66,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${playfair.variable} ${nunito.variable}`}>
+      <head>
+        <link rel="alternate" href="https://oleksandrpryvalov.art/" hrefLang="en" />
+        <link rel="alternate" href="https://oleksandrpryvalov.art/" hrefLang="fr" />
+      </head>
       <body>
         <ThemeProvider>
           <PaintingProvider>
-          <LanguageProvider>
-            <LayoutWrapper isLoading={false}>{children}</LayoutWrapper>
+            <LanguageProvider>
+              <LayoutWrapper isLoading={false}>{children}</LayoutWrapper>
             </LanguageProvider>
           </PaintingProvider>
         </ThemeProvider>
