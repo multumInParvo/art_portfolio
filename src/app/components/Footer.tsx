405fbdcf0173e9063da7cdd7a28c829c33bcb8bd
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-11-21 19:39:43 +0100

    Add link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 
 const Footer: React.FC = () => {
   return (
     <footer className="text-xs font-nunito p-5 md:p-0 md:mt-8">
-      © 2024 by Oleksandr Pryvalov
+      <Link href="https://oleksandrpryvalov.com/" target="_blank" rel="noopener noreferrer">
+        © 2024 by Oleksandr Pryvalov
+      </Link>
     </footer>
   );
 };
