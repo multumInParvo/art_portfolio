04a8a263cbf5dcb4e1bff27bcfd63abdd0b20db8
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-11-24 00:55:44 +0100

    Add provisionary images

--- src/app/data/paintings.tsx
+++ src/app/data/paintings.tsx
@@ -12,8 +12,8 @@ export type Painting = {
   // Paintings array with the type annotation
   export const paintings: Painting[] = [
     {
-      src: "/images/Work_in_progress_06.jpeg",
-      additionalImages: ['/images/Work_in_progress_01.jpeg', '/images/Work_in_progress_02.jpeg', '/images/Work_in_progress_03.jpeg','/images/Work_in_progress_04.jpeg','/images/Work_in_progress_05.jpeg'],
+      src: "/images/unexpected_encounter_01.jpeg",
+      additionalImages: ['/images/unexpected_encounter_06.jpg', '/images/unexpected_encounter_04.jpeg', '/images/unexpected_encounter_05.jpeg','/images/unexpected_encounter_02.jpeg','/images/unexpected_encounter_03.jpg'],
       title: "An unexpected encounter",
       description: "owl and orchidea",
       dimensions: "60 x 80 cm",
