9fa38fe6747029f94ef1adf3303dbe1be7e8579c
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-11-23 19:53:18 +0100

    Add transition duration to icons effect

--- src/app/components/DesktopSidebar.tsx
+++ src/app/components/DesktopSidebar.tsx
@@ -53,7 +53,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
                             <div className="relative">
                                 <Link
                                     href="/about"
-                                    className="font-nunito inline-block relative after:content-[''] after:absolute after:-bottom-0 after:left-0 after:w-full after:h-[2px] after:scale-x-0 after:bg-gray-700 dark:after:bg-darkGold after:transition-transform after:duration-300 hover:after:scale-x-100"
+                                    className="font-nunito inline-block relative after:content-[''] after:absolute after:-bottom-0 after:left-0 after:w-full after:h-[2px] after:scale-x-0 after:bg-gray-700 dark:after:bg-darkGold after:transition-transform after:duration-300 ease-in-out hover:after:scale-x-100"
                                 >
                                     {translations.about}
                                 </Link>
@@ -62,7 +62,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
                             <div className="relative">
                                 <Link
                                     href="/contact"
-                                    className="font-nunito inline-block relative after:content-[''] after:absolute after:-bottom-0 after:left-0 after:w-full after:h-[2px] after:scale-x-0 after:bg-gray-700 dark:after:bg-darkGold after:transition-transform after:duration-300 hover:after:scale-x-100"
+                                    className="font-nunito inline-block relative after:content-[''] after:absolute after:-bottom-0 after:left-0 after:w-full after:h-[2px] after:scale-x-0 after:bg-gray-700 dark:after:bg-darkGold after:transition-transform after:duration-300 ease-in-out hover:after:scale-x-100"
                                 >
                                     contact
                                 </Link>
@@ -74,14 +74,14 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
                                     target="_blank"
                                     rel="noopener noreferrer"
                                     aria-label="Instagram Profile - Oleksandr Pryvalov"
-                                    className="flex items-center justify-center w-5 h-5 rounded-full focus:outline-none hover:shadow-[2px_2px_8px_2px_rgba(0,0,0,0.3)] dark:hover:shadow-[2px_2px_8px_2px_rgba(255,255,255,0.25)]"
+                                    className="flex items-center justify-center w-5 h-5 rounded-full focus:outline-none hover:shadow-[2px_2px_8px_2px_rgba(0,0,0,0.3)] dark:hover:shadow-[2px_2px_8px_2px_rgba(255,255,255,0.25)] transition-shadow duration-300 ease-in-out"
                                 >
                                     <FaInstagram className="text-base hover:text-gray-900 dark:hover:text-gray-200" aria-hidden="true" />
                                 </Link>
 
                                 <button
                                     onClick={toggleLanguage}
-                                    className="rounded-full w-5 h-5 focus:outline-none text-xs font-medium hover:shadow-[2px_2px_8px_2px_rgba(0,0,0,0.3)] dark:hover:shadow-[2px_2px_8px_2px_rgba(255,255,255,0.25)]"
+                                    className="rounded-full w-5 h-5 focus:outline-none text-xs font-medium hover:shadow-[2px_2px_8px_2px_rgba(0,0,0,0.3)] dark:hover:shadow-[2px_2px_8px_2px_rgba(255,255,255,0.25)] transition-shadow duration-300"
                                     aria-label={translations.switch_language}
                                 >
                                     {language}
@@ -89,7 +89,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
 
                                 <button
                                     onClick={toggleTheme}
-                                    className="flex justify-center items-center rounded-full w-5 h-5 focus:outline-none active:duration-75 hover:shadow-[2px_2px_8px_2px_rgba(0,0,0,0.3)] dark:hover:shadow-[2px_2px_8px_2px_rgba(255,255,255,0.25)]"
+                                    className="flex justify-center items-center rounded-full w-5 h-5 focus:outline-none active:duration-75 hover:shadow-[2px_2px_8px_2px_rgba(0,0,0,0.3)] dark:hover:shadow-[2px_2px_8px_2px_rgba(255,255,255,0.25)] transition-shadow duration-300"
                                     aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
                                 >
                                     {theme === 'light' ? <Moon className="w-4 h-4" /> : <Sun className="w-4 h-4" />}
