b6c1c86989eda4175502027540bb76c520eb1658
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-12-30 19:55:49 +0100

    Update app

--- src/app/data/paintings.tsx
+++ src/app/data/paintings.tsx
@@ -13,7 +13,7 @@ export type Painting = {
   export const paintings: Painting[] = [
     {
       src: "/images/prickly_distance_01.webp",
-      additionalImages: ['/images/prickly_distance_01.webp', '/images/prickly_distance_01.webp','/images/prickly_distance_01.webp','/images/prickly_distance_01.webp'],
+      additionalImages: ['/images/prickly_distance_02.webp', '/images/prickly_distance_03.webp','/images/prickly_distance_04.webp','/images/prickly_distance_05.webp'],
       title: "Prickly Distance",
       description: "owl and cactus",
       dimensions: "60 x 80 cm",
@@ -30,46 +30,19 @@ export type Painting = {
       year: 2024
     },
     {
-      src: "/images/azaylerideau_01.webp",
-      additionalImages: ['/images/azaylerideau_02.webp', '/images/azaylerideau_03.webp', '/images/azaylerideau_04.webp'],
-      title: "Azay-le-Rideau",
-      description: "The house with red doors",
-      dimensions: "60 x 80 cm",
-      material: "oil on canvas",
-      year: 2024
-    },
-    {
-      src: "/images/campestre.webp",
-      additionalImages: [],
-      title: "Campestre",
-      description: "Cheese and Milk",
-      dimensions: "30 x 40 cm",
-      material: "oil on canvas",
-      year: 2022
-    },
-    {
-      src: "/images/smoldering_promise_01.webp",
-      additionalImages: ['/images/smoldering_promise_02.webp', '/images/smoldering_promise_03.webp', '/images/smoldering_promise_04.webp'],
-      title: "Smoldering promise",
-      description: "Candle, owl, plant",
-      dimensions: "60 x 80 cm",
-      material: "oil on canvas",
-      year: 2024
-    },
-    {
-      src: "/images/campestre_2.webp",
-      additionalImages: [],
-      title: "Campestre 2",
-      description: "Apples",
-      dimensions: "30 x 40 cm",
-      material: "oil on canvas",
-      year: 2022
-    },
-    {
       src: "/images/leaving_the_nest_01.webp",
       additionalImages: ['/images/leaving_the_nest_02.webp', '/images/leaving_the_nest_03.webp', '/images/leaving_the_nest_04.webp'],
       title: "Leaving the nest",
       description: "Red Chicken",
+      dimensions: "60 x 80 cm",
+      material: "oil on canvas",
+      year: 2024
+    },
+    {
+      src: "/images/smoldering_promise_02.webp",
+      additionalImages: ['/images/smoldering_promise_01.webp', '/images/smoldering_promise_03.webp', '/images/smoldering_promise_04.webp'],
+      title: "Smoldering promise",
+      description: "Candle, owl, plant",
       dimensions: "60 x 80 cm",
       material: "oil on canvas",
       year: 2024
